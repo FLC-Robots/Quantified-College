3cbc9b05c54896a776ac9266111381b01aa686b4
troop129 <trooprasheed@gmail.com>
2023-03-29 12:45:07 -0700

    added gitignore, test mathplotlib

--- Backend/main.py
+++ Backend/main.py
@@ -1,8 +1,11 @@
-from flask import Flask, render_template, request, redirect, url_for, flash
-import matplotlib.pyplot as plt
-import data_processor as dp
+from flask import Flask, render_template, request, redirect, url_for, flash, jsonify
+import data_processor as data_p
 
+import base64
+from io import BytesIO
+from matplotlib.figure import Figure
 
+dp = data_p.data_processor()
 app = Flask(__name__)
 
 @app.route('/')
@@ -17,6 +20,18 @@ def status():
 def poc():
     data = "Hello World!"
     return render_template('poc.html', data=data)
+
+@app.route('/test')
+def test():
+    fig = Figure()
+    ax = fig.subplots()
+    ax.plot([1, 2])
+    # Save it to a temporary buffer.
+    buf = BytesIO()
+    fig.savefig(buf, format="png")
+    # Embed the result in the html output.
+    data = base64.b64encode(buf.getbuffer()).decode("ascii")
+    return render_template('test.html', data=data)
 
 def run():
     app.run(host='localhost', port=5000, debug=True)
