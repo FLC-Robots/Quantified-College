fd9feab366c614ade7030f4271e26e072b5e3f58
troop129 <trooprasheed@gmail.com>
2023-04-05 10:41:54 -0700

    added automatic LAN hosting

--- Backend/main.py
+++ Backend/main.py
@@ -31,7 +31,8 @@ def data():
     return jsonify(num=random.randint(0, 100))
 
 def run():
-    app.run(host='localhost', port=5000, debug=True)
+
+    app.run(host='0.0.0.0', port=5000, debug=True)
 
 if __name__ == '__main__':
     run()
